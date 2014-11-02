a1ff697808c0b4b8542f676b1dcf3410befeed0f
Gerson Carlos <gerson.mtg@gmail.com>
2014-11-01 23:42:48 -0200

    Adding reading instructions.

--- ServiceLane/ServiceLane.scala
+++ ServiceLane/ServiceLane.scala
@@ -1,5 +1,12 @@
 object ServiceLane {
   def main(args: Array[String]) {
-    println("Hello World")
+    val Array(n,t) = scala.io.StdIn.readLine.split(" ").map(_.toInt)
+    val widthArray = scala.io.StdIn.readLine.split(" ").map(_.toInt)
+
+    var _ = 0
+    for(_ <- 0 until t) {
+      val Array(i,j) = scala.io.StdIn.readLine.split(" ").map(_.toInt)
+      println("calculation for (" + i + "," + j + ")")
+    }
   }
 }
