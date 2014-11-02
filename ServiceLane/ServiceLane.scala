1e017a4ddb27507280c863aeff264d49e24d5b3b
Gerson Carlos <gerson.mtg@gmail.com>
2014-11-02 11:13:12 -0200

    Added calculation logic.

--- ServiceLane/ServiceLane.scala
+++ ServiceLane/ServiceLane.scala
@@ -1,4 +1,17 @@
 object ServiceLane {
+  // calculate the max unit
+  def calcMaxUnit(i:Int, j:Int, widthArray:Array[Int]) : Int = {
+    var position = 0
+    var maxUnit = 3
+    for(position <- i to j) {
+      if(widthArray(position) < maxUnit) {
+        maxUnit = widthArray(position)
+      }
+    }
+    return maxUnit
+  }
+
+  // main function
   def main(args: Array[String]) {
     val Array(n,t) = scala.io.StdIn.readLine.split(" ").map(_.toInt)
     val widthArray = scala.io.StdIn.readLine.split(" ").map(_.toInt)
@@ -6,7 +19,7 @@ object ServiceLane {
     var _ = 0
     for(_ <- 0 until t) {
       val Array(i,j) = scala.io.StdIn.readLine.split(" ").map(_.toInt)
-      println("calculation for (" + i + "," + j + ")")
+      println(calcMaxUnit(i,j,widthArray))
     }
   }
 }
